00f7050d9cb169b16030675ff95dd78a7a5d2230
roflmao <c.arleryd@gmail.com>
2014-09-16 15:47:19 +0200

    changed stuff

--- Game.cpp
+++ Game.cpp
@@ -17,12 +17,8 @@ Game::Game(sf::RenderWindow* window_) {
     
     // Construct a world object, which will hold and simulate the rigid bodies.
     world = new b2World(gravity);
-
-//    std::cout << "Window:" << std::endl << "width: " << window->getSize().x << std::endl << "height: " << window->getSize().y << std::endl;
     
     boxes.push_back(new Rectangle(world, new b2Vec2(750.0f, 50.0f), new b2Vec2(0.0f, -float(window->getSize().y)/2), false));
-//	boxes.push_back(new Rectangle(world, new b2Vec2(100.0f, 100.0f), new b2Vec2(0.0f, 200.0f), true));
-//    boxes.push_back(new Rectangle(world, new b2Vec2(50.0f, 50.0f), new b2Vec2(-50.0f, 100.0f),  true));
     
     player = new Player(world);
 }
